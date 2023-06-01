229de8c47ac1bbba8930cd7cb4de57547c365cf3
unknown <Shikder-Vai>
2023-06-01 23:55:00 +0600

    some comments and types added

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,16 @@
-import express, { Application } from "express";
+import express, { Application, Request, Response } from "express";
 import cors from "cors";
 const app: Application = express();
 
+//cors setup
 app.use(cors());
 
 //parser
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
+//testing
+app.get("/", (req: Request, res: Response) => {
   res.send("application successfully working");
 });
 
